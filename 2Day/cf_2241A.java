8fa6ac55ee6bdcbcfcfff5387e103e5e80798d05
yoneshmurugan <yoneshmurugan@gmail.com>
2026-07-26 11:47:14 +0530

    day 2 in code force

---
+++ 2Day/cf_2241A.java
@@ -0,0 +1,22 @@
+// Problem: Divide and Conquer
+// Contest: 2241 | Index: A
+// URL: https://codeforces.com/contest/2241/problem/A
+// Rating: 800 | Tags: greedy, math, number theory, *800
+// Memory: 256 megabytes | Time: 1 second
+// Author: 
+import java.util.Scanner;
+public class cf_2241A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int input =  sc.nextInt();
+        while (input-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if (x % y == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}

5ed48e32b6680a31541b60df79a616f08641a03b
yoneshmurugan <yoneshmurugan@gmail.com>
2026-07-27 14:31:10 +0530

    feat: add solution for Codeforces problem 791A Bear and Big Brother

---
+++ 3Day/cf_791A.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class cf_791A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int year = 0;
+
+            while (num1 <= num2) {
+                year++;
+                num1 = 3 * num1;
+                num2 = 2 * num2;
+            }
+            System.out.println(year);
+            sc.close();
+        }
+    }

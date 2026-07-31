b3ea1d60b01b16478ec27018a8e91eecfe863b9f
yoneshmurugan <yoneshmurugan@gmail.com>
2026-07-31 11:15:43 +0530

    feat: add solution for Codeforces problem 1030A "In Search of an Easy Problem"

---
+++ 7Day/cf_1030A.java
@@ -0,0 +1,20 @@
+// Problem: In Search of an Easy Problem
+// Contest: 1030 | Index: A
+// URL: https://codeforces.com/contest/1030/problem/A
+// Rating: 800 | Tags: implementation, *800
+// Memory: 256 megabytes | Time: 1 second
+// Author: 
+import java.util.Scanner;
+public class cf_1030A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int people = sc.nextInt();
+        for(int i = 0; i < people; i++){
+            int opinion = sc.nextInt();
+            if (opinion ==1){
+                System.out.println("HARD");
+                return;
+            }
+    }
+    System.out.println("EaSy");
+}}
